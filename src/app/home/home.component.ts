8f542fadb8fa3d470f92c2710168d8cc6f9464ae
johnson790 <johnsonykyando1739@gmail.com>
2023-09-08 10:36:58 +0300

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
-import {Component, OnInit} from '@angular/core';
-import {Course, sortCoursesBySeqNo} from '../model/course';
-import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
-import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
-
+import { Component, OnInit } from '@angular/core';
+import { Course, sortCoursesBySeqNo } from '../model/course';
+import { interval, noop, Observable, of, throwError, timer } from 'rxjs';
+import {
+  catchError,
+  delay,
+  delayWhen,
+  filter,
+  finalize,
+  map,
+  retryWhen,
+  shareReplay,
+  tap
+} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'home',
@@ -14,47 +24,35 @@ import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 })
 export class HomeComponent implements OnInit {
 
-  beginnerCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
+  advancedCourses$: Observable<Course[]>;
 
-
-  constructor(private http: HttpClient, private dialog: MatDialog) {
-
-  }
+  constructor(private coursesService: CoursesService, private dialog: MatDialog) {}
 
   ngOnInit() {
+    const courses$ = this.coursesService.loadAllCourses().pipe(
+      map(courses => courses.sort(sortCoursesBySeqNo))
+    );
 
-    this.http.get('/api/courses')
-      .subscribe(
-        res => {
+    this.beginnerCourses$ = courses$.pipe(
+      map(courses => courses.filter(course => course.category === 'BEGINNER'))
+    );
 
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
-
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-
-        });
-
+    this.advancedCourses$ = courses$.pipe(
+      map(courses => courses.filter(course => course.category === 'ADVANCED'))
+    );
   }
 
   editCourse(course: Course) {
-
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
+    dialogConfig.width = '400px';
 
     dialogConfig.data = course;
 
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
   }
-
 }
-
-
-
-
---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Course } from '../model/course'; // Adjust the path as needed
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CoursesService {
+    constructor(private http: HttpClient) {}
+
+    loadAllCourses(): Observable<Course[]> { // Corrected the type to 'Course[]'
+        return this.http.get<Course[]>("/api/courses")
+            .pipe(
+                map(res => res["payload"])
+            );
+    }
+}
